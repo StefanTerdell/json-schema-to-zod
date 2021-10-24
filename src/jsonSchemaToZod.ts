c39a4eadd62ebf24a62f85d07522f0413372b914
Stefan Terdell <stefan.terdell@live.com>
2021-10-24 20:03:13 +0200

    ✨ Export schema from result with optional name

--- src/jsonSchemaToZod.ts
+++ src/jsonSchemaToZod.ts
@@ -2,5 +2,9 @@ import { JSONSchema7 } from "json-schema";
 import { format } from "prettier";
 import { parseSchema } from "./parseSchema";
 
-
-export const jsonSchemaToZod = (schema: JSONSchema7): string => format(`import {z} from 'zod'\n\n${parseSchema(schema)}`);
+export const jsonSchemaToZod = (schema: JSONSchema7, name?: string): string =>
+  format(
+    `import {z} from 'zod'\n\nexport ${
+      name ? `const ${name}=` : "default "
+    }${parseSchema(schema)}`
+  );
