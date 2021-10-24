173e4c3e74a9a0a6cc63b12ba162da7445569c99
Stefan Terdell <stefan.terdell@live.com>
2021-10-24 22:39:17 +0200

    ✨ Tuple support

--- src/parsers/parseArray.ts
+++ src/parsers/parseArray.ts
@@ -4,6 +4,8 @@ import { parseSchema } from "./parseSchema";
 export const parseArray = (schema: JSONSchema7 & { type: "array" }) => {
   let r = !schema.items
     ? "z.array(z.any())"
+    : Array.isArray(schema.items)
+    ? `z.tuple([${schema.items.map(parseSchema)}])`
     : `z.array(${parseSchema(schema.items)})`;
   if (schema.minItems !== undefined) r += `.min(${schema.minItems})`;
   if (schema.maxItems !== undefined) r += `.max(${schema.maxItems})`;
