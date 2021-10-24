0639700eb1ab1710d82eb9ee65ce0ba8939114f3
Stefan Terdell <stefan.terdell@live.com>
2021-10-24 23:20:27 +0200

    🐛 fix broken call

--- src/parsers/parseObject.ts
+++ src/parsers/parseObject.ts
@@ -8,7 +8,7 @@ export const parseObject = (schema: JSONSchema7 & { type: "object" }) => {
   return !schema.properties
     ? typeof schema.additionalProperties === "object"
       ? `z.record(${parseSchema(schema.additionalProperties)})`
-      : "z.object().catchall(z.any())"
+      : "z.object({}).catchall(z.any())"
     : `z.object({${Object.entries(schema?.properties).map(
         ([k, v]) =>
           `'${k}':${parseSchema(v)}${
