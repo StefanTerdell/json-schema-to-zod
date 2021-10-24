2ca18d4cdf69de3c286b95bfb3343f21791d2b9a
Stefan Terdell <stefan.terdell@live.com>
2021-10-24 20:26:33 +0200

    🎨 Implement improvements

--- src/parseSchema.ts
+++ src/parseSchema.ts
@@ -1,12 +1,17 @@
+import { parseAnyOf } from "./parsers/parseAnyOf";
+import { parseBoolean } from "./parsers/parseBoolean";
+import { parseDefault } from "./parsers/parseDefault";
+import { parseMultipleType } from "./parsers/parseMultipleType";
+import { parseNot } from "./parsers/parseNot";
+import { parseNull } from "./parsers/parseNull";
+import { parseAllOf } from "./parsers/parseAllOf";
 import { parseArray } from "./parsers/parseArray";
+import { parseConst } from "./parsers/parseConst";
 import { parseEnum } from "./parsers/parseEnum";
+import { parseIfThenElse } from "./parsers/parseIfThenElse";
 import { parseNumber } from "./parsers/parseNumber";
 import { parseObject } from "./parsers/parseObject";
-import { parseAllOf } from "./parsers/parseAllOf";
 import { parseString } from "./parsers/parseString";
-import { parseIfThenElse } from "./parsers/parseIfThenElse";
-
-//TODO: const
 
 export const parseSchema = (schema: any): string => {
   if (schema.type === "object") {
@@ -14,26 +19,28 @@ export const parseSchema = (schema: any): string => {
   } else if (schema.type === "array") {
     return parseArray(schema);
   } else if (Array.isArray(schema.type)) {
-    return `z.union([${schema.type.map(parseSchema)}])`;
+    return parseMultipleType(schema);
   } else if (schema.anyOf) {
-    return `z.union([${schema.anyOf.map(parseSchema)}])`;
+    return parseAnyOf(schema);
   } else if (schema.allOf) {
-    return parseAllOf(schema.allOf);
+    return parseAllOf(schema);
   } else if (schema.not) {
-    return "z.undefined()";
+    return parseNot(schema);
   } else if (schema.enum) {
     return parseEnum(schema); //<-- needs to come before primitives
+  } else if (schema.const) {
+    return parseConst(schema);
   } else if (schema.type === "string") {
     return parseString(schema);
   } else if (schema.type === "number") {
     return parseNumber(schema);
   } else if (schema.type === "boolean") {
-    return "z.boolean()";
+    return parseBoolean(schema);
   } else if (schema.type === "null") {
-    return "z.null()";
+    return parseNull(schema);
   } else if (schema.if && schema.then && schema.else) {
     return parseIfThenElse(schema);
   } else {
-    return "z.any()";
+    return parseDefault(schema);
   }
 };
