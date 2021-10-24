8c2547842155adacd7ac3e4f74d2631796a2ac17
Stefan Terdell <stefan.terdell@live.com>
2021-10-24 20:04:11 +0200

    🎨 Improve structure pending options oject

--- src/parsers/parseObject.ts
+++ src/parsers/parseObject.ts
@@ -1,15 +1,23 @@
 import { parseSchema } from "../parseSchema";
 
+const requiredFlag = ""; //".required()"
+const defaultAdditionalFlag = ""; //".strip()"
+
 export const parseObject = (schema: any) => {
-    return !schema.properties && typeof schema.additionalProperties === "object"
-        ? `z.record(${parseSchema(schema.additionalProperties)})`
-        : `z.object({${Object.entries(schema?.properties).map(
-            ([k, v]) => `'${k}':${parseSchema(v)}${schema.required?.includes(k) ? ".required()" : ".optional()"}`
-        )}})${schema.additionalProperties === true
-            ? ".catchall(z.any())"
-            : schema.additionalProperties === false
-                ? ".strict()"
-                : typeof schema.additionalProperties === "object"
-                    ? `.catchall(${parseSchema(schema.additionalProperties)})`
-                    : ".strip()"}`;
+  return !schema.properties && typeof schema.additionalProperties === "object"
+    ? `z.record(${parseSchema(schema.additionalProperties)})`
+    : `z.object({${Object.entries(schema?.properties).map(
+        ([k, v]) =>
+          `'${k}':${parseSchema(v)}${
+            schema.required?.includes(k) ? requiredFlag : ".optional()"
+          }`
+      )}})${
+        schema.additionalProperties === true
+          ? ".catchall(z.any())"
+          : schema.additionalProperties === false
+          ? ".strict()"
+          : typeof schema.additionalProperties === "object"
+          ? `.catchall(${parseSchema(schema.additionalProperties)})`
+          : defaultAdditionalFlag
+      }`;
 };
