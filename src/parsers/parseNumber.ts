8d8e90e32efe76f2fe372c7a14a4ec022d4e4a29
Stefan Terdell <stefan.terdell@live.com>
2021-10-24 20:03:55 +0200

    ✨fix faulty property and add exclusive number validation

--- src/parsers/parseNumber.ts
+++ src/parsers/parseNumber.ts
@@ -1,10 +1,16 @@
-export const parseNumber = (schema: any) => {
-    let r = "z.number()";
-    if (schema.format === "int64")
-        r += ".int()";
-    if (schema.min !== undefined)
-        r += `.min(${schema.min})`;
-    if (schema.max !== undefined)
-        r += `.max(${schema.max})`;
-    return r;
+import { JSONSchema7 } from "json-schema";
+
+export const parseNumber = (schema: JSONSchema7) => {
+  let r = "z.number()";
+  if (schema.format === "int64" || schema.multipleOf === 1) r += ".int()";
+  if (schema.multipleOf !== undefined && schema.multipleOf !== 1) {
+    r += `.multipleOf(${schema.multipleOf})`;
+  }
+  if (schema.minimum !== undefined) r += `.gte(${schema.minimum})`;
+  if (schema.maximum !== undefined) r += `.lte(${schema.maximum})`;
+  if (schema.exclusiveMinimum !== undefined)
+    r += `.gt(${schema.exclusiveMinimum})`;
+  if (schema.exclusiveMaximum !== undefined)
+    r += `.lt(${schema.exclusiveMaximum})`;
+  return r;
 };
