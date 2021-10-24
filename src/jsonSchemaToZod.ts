cae9ab140cf2ed58d0dd1247c890675729fac25c
Stefan Terdell <stefan.terdell@live.com>
2021-10-24 21:01:32 +0200

    ✨ improve API

--- src/jsonSchemaToZod.ts
+++ src/jsonSchemaToZod.ts
@@ -1,9 +1,14 @@
 import { JSONSchema7 } from "json-schema";
 import { parseSchema } from "./parsers/parseSchema";
+import { format } from "./utils/format";
 
-export const jsonSchemaToZod = (schema: JSONSchema7, name?: string): string =>
+export const jsonSchemaToZod = (
+  schema: JSONSchema7,
+  name?: string,
+  module: boolean = true
+): string =>
   format(
-    `import {z} from 'zod'\n\nexport ${
-      name ? `const ${name}=` : "default "
+    `${module ? `import {z} from 'zod'\n\nexport ` : ""}${
+      name ? `const ${name}=` : module ? "default " : "const schema="
     }${parseSchema(schema)}`
   );
