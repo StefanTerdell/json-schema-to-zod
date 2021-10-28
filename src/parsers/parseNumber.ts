b8b736602fd0ed05f33121c3f2fae45ff9615c7a
Stefan Terdell <stefan.terdell@live.com>
2021-10-28 22:49:37 +0200

    ✨ Backwards compatibility with older json schemas

--- src/parsers/parseNumber.ts
+++ src/parsers/parseNumber.ts
@@ -8,16 +8,38 @@ export const parseNumber = (
     schema.format === "int64" ||
     schema.multipleOf === 1 ||
     schema.type === "integer"
-  )
+  ) {
     r += ".int()";
+  }
+  
   if (schema.multipleOf !== undefined && schema.multipleOf !== 1) {
     r += `.multipleOf(${schema.multipleOf})`;
   }
-  if (schema.minimum !== undefined) r += `.gte(${schema.minimum})`;
-  if (schema.maximum !== undefined) r += `.lte(${schema.maximum})`;
-  if (schema.exclusiveMinimum !== undefined)
+
+  if (schema.minimum !== undefined) {
+    if ((schema as any).exclusiveMinimum === true) {
+      r += `.gt({${schema.minimum}})`;
+    } else {
+      r += `.gte(${schema.minimum})`;
+    }
+  } else if (
+    schema.exclusiveMinimum !== undefined &&
+    (schema as any).exclusiveMinimum !== false
+  ) {
     r += `.gt(${schema.exclusiveMinimum})`;
-  if (schema.exclusiveMaximum !== undefined)
+  }
+
+  if (schema.maximum !== undefined) {
+    if ((schema as any).exclusiveMaximum === true) {
+      r += `.lt({${schema.maximum}})`;
+    } else {
+      r += `.lte(${schema.maximum})`;
+    }
+  } else if (
+    schema.exclusiveMaximum !== undefined &&
+    (schema as any).exclusiveMaximum !== false
+  ) {
     r += `.lt(${schema.exclusiveMaximum})`;
+  }
   return r;
 };
