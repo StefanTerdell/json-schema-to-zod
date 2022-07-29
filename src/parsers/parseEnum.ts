69d2a8f1fe2d1d141d0d2955cc5dab53e7fb5452
Stefan Terdell <stefan.terdell@live.com>
2022-07-29 18:00:07 +0200

    🐛 Support all types of enum values

--- src/parsers/parseEnum.ts
+++ src/parsers/parseEnum.ts
@@ -4,6 +4,8 @@ export const parseEnum = (
   schema: JSONSchema7 & { enum: JSONSchema7Type[] | JSONSchema7Type }
 ) => {
   return Array.isArray(schema.enum)
-    ? `z.enum([${schema.enum.map((x: any) => JSON.stringify(x))}])`
+    ? schema.enum.every((x) => typeof x === "string")
+      ? `z.enum([${schema.enum.map((x) => JSON.stringify(x))}])`
+      : `z.union([${schema.enum.map((x) => `z.literal(${JSON.stringify(x)})`)}])`
     : `z.literal(${JSON.stringify(schema.enum)})`;
 };
