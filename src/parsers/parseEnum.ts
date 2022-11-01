8e67bda92f8221a674f50e985d93baf03ca2662b
werifu <wohu1363195380@outlook.com>
2022-11-01 22:35:11 +0800

    fix: enum error when there is only one element

--- src/parsers/parseEnum.ts
+++ src/parsers/parseEnum.ts
@@ -3,9 +3,15 @@ import { JSONSchema7, JSONSchema7Type } from "json-schema";
 export const parseEnum = (
   schema: JSONSchema7 & { enum: JSONSchema7Type[] | JSONSchema7Type }
 ) => {
-  return Array.isArray(schema.enum)
-    ? schema.enum.every((x) => typeof x === "string")
-      ? `z.enum([${schema.enum.map((x) => JSON.stringify(x))}])`
-      : `z.union([${schema.enum.map((x) => `z.literal(${JSON.stringify(x)})`)}])`
-    : `z.literal(${JSON.stringify(schema.enum)})`;
+  if (Array.isArray(schema.enum)) {
+    if (schema.enum.every((x) => typeof x === 'string')) {
+      return `z.enum([${schema.enum.map((x) => JSON.stringify(x))}])`;
+    } else if (schema.enum.length === 1) {  // union does not work when there is only one element
+      return `z.literal(${JSON.stringify(schema.enum[0])})`;
+    } else {
+      return `z.union([${schema.enum.map((x) => `z.literal(${JSON.stringify(x)})`)}])`;
+    }
+  } else {
+    return `z.literal(${JSON.stringify(schema.enum)})`;
+  }
 };
