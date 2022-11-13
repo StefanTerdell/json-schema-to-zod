5604e4248dd2abf07393c6e884545eb7ea453ab7
Lars Strojny <lars@strojny.net>
2022-11-13 23:03:13 +0100

    Add tests for default behavior

--- test/jsonSchemaToZod.test.ts
+++ test/jsonSchemaToZod.test.ts
@@ -1,12 +1,62 @@
-import jsonSchemaToZod from '../src'
+import jsonSchemaToZod from "../src";
 
 describe("jsonSchemaToZod", () => {
-    it("should produce a string of JS code creating a Zod schema from a simple JSON schema", () => {
-        expect(jsonSchemaToZod({
-            type: "string"
-        })).toStrictEqual(`import { z } from "zod";
+  it("should produce a string of JS code creating a Zod schema from a simple JSON schema", () => {
+    expect(
+      jsonSchemaToZod({
+        type: "string",
+      })
+    ).toStrictEqual(`import { z } from "zod";
 
 export default z.string();
-`)
-    })
-})
+`);
+  });
+
+  it("should include defaults", () => {
+    expect(
+      jsonSchemaToZod({
+        type: "string",
+        default: "foo",
+      })
+    ).toStrictEqual(`import { z } from "zod";
+
+export default z.string().default("foo");
+`);
+  });
+
+  it("can exclude defaults", () => {
+    expect(
+      jsonSchemaToZod(
+        {
+          type: "string",
+          default: "foo",
+        },
+        undefined,
+        true,
+        true
+      )
+    ).toStrictEqual(`import { z } from "zod";
+
+export default z.string();
+`);
+  });
+
+  it("will remove optionality if default is present", () => {
+    expect(
+      jsonSchemaToZod(
+        {
+          type: "object",
+          properties: {
+              prop: {
+                  type: "string",
+                  default: 'def'
+              }
+          }
+        }
+      )
+    ).toStrictEqual(`import { z } from "zod";
+
+export default z.object({ prop: z.string().default("def") });
+`);
+  });
+});
