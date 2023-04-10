c9ac7231f266a87050d33219a3b51502eaeeabb0
Dmitry Zakharov <dzakh.dev@gmail.com>
2023-04-10 16:40:09 +0400

    Fix parseEnum

--- src/parsers/parseEnum.ts
+++ src/parsers/parseEnum.ts
@@ -1,20 +1,20 @@
 import { JSONSchema7, JSONSchema7Type } from "json-schema";
 
 export const parseEnum = (
-  schema: JSONSchema7 & { enum: JSONSchema7Type[] | JSONSchema7Type }
+  schema: JSONSchema7 & { enum: JSONSchema7Type[] }
 ) => {
-  if (Array.isArray(schema.enum)) {
+  if (schema.enum.length === 0) {
+    return `z.any()`;
+  } else if (schema.enum.length === 1) {
+    // union does not work when there is only one element
+    return `z.literal(${JSON.stringify(schema.enum[0])})`;
+  } else {
     if (schema.enum.every((x) => typeof x === "string")) {
       return `z.enum([${schema.enum.map((x) => JSON.stringify(x))}])`;
-    } else if (schema.enum.length === 1) {
-      // union does not work when there is only one element
-      return `z.literal(${JSON.stringify(schema.enum[0])})`;
     } else {
       return `z.union([${schema.enum.map(
         (x) => `z.literal(${JSON.stringify(x)})`
       )}])`;
     }
-  } else {
-    return `z.literal(${JSON.stringify(schema.enum)})`;
   }
 };
