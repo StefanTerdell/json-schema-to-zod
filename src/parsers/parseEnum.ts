55d5271c1283a93f728cd801425ff94c00c767a1
Stefan Terdell <stefan.terdell@live.com>
2023-04-30 14:44:49 +0200

    🐛 Empty enum means never where valid

--- src/parsers/parseEnum.ts
+++ src/parsers/parseEnum.ts
@@ -4,17 +4,15 @@ export const parseEnum = (
   schema: JSONSchema7 & { enum: JSONSchema7Type[] }
 ) => {
   if (schema.enum.length === 0) {
-    return `z.any()`;
+    return "z.never()";
   } else if (schema.enum.length === 1) {
     // union does not work when there is only one element
     return `z.literal(${JSON.stringify(schema.enum[0])})`;
+  } else if (schema.enum.every((x) => typeof x === "string")) {
+    return `z.enum([${schema.enum.map((x) => JSON.stringify(x))}])`;
   } else {
-    if (schema.enum.every((x) => typeof x === "string")) {
-      return `z.enum([${schema.enum.map((x) => JSON.stringify(x))}])`;
-    } else {
-      return `z.union([${schema.enum.map(
-        (x) => `z.literal(${JSON.stringify(x)})`
-      )}])`;
-    }
+    return `z.union([${schema.enum.map(
+      (x) => `z.literal(${JSON.stringify(x)})`
+    )}])`;
   }
 };
