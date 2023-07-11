42f397bbd43dc60179592937c542a6da9b4c71cd
Sylvain Perron <sylvain.perron@botpress.com>
2023-07-11 15:36:15 -0400

    cleanup

--- src/parsers/parseDiscriminator.ts
+++ src/parsers/parseDiscriminator.ts
@@ -22,39 +22,3 @@ export const parseDiscriminator = (
     schema.discriminator?.propertyName
   }", [${schemas.join(", ")}])`;
 };
-
-export const __original = (
-  schema: JSONSchema7Extended & { oneOf: JSONSchema7Definition[] },
-  refs: Refs
-) => {
-  return schema.oneOf.length
-    ? schema.oneOf.length === 1
-      ? parseSchema(schema.oneOf[0], {
-          ...refs,
-          path: [...refs.path, "oneOf", 0],
-        })
-      : `z.any().superRefine((x, ctx) => {
-    const schemas = [${schema.oneOf.map((schema, i) =>
-      parseSchema(schema, {
-        ...refs,
-        path: [...refs.path, "oneOf", i],
-      })
-    )}];
-    const errors = schemas.reduce(
-      (errors: z.ZodError[], schema) =>
-        ((result) => ("error" in result ? [...errors, result.error] : errors))(
-          schema.safeParse(x)
-        ),
-      []
-    );
-    if (schemas.length - errors.length !== 1) {
-      ctx.addIssue({
-        path: ctx.path,
-        code: "invalid_union",
-        unionErrors: errors,
-        message: "Invalid input: Should pass single schema",
-      });
-    }
-  })`
-    : "z.any()";
-};
