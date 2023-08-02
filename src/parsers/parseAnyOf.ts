6b5875fc8f4d7f650508e46e47fd6a4a79ece15d
Fabian <spiele2014fs@gmail.com>
2023-08-02 14:32:05 +0200

    .nullable()

--- src/parsers/parseAnyOf.ts
+++ src/parsers/parseAnyOf.ts
@@ -1,19 +1,32 @@
 import { JSONSchema7, JSONSchema7Definition } from "json-schema";
 import { Refs } from "../Types";
-import { parseSchema } from "./parseSchema";
+import { its, parseSchema } from "./parseSchema";
 
 export const parseAnyOf = (
-  schema: JSONSchema7 & { anyOf: JSONSchema7Definition[] },
-  refs: Refs
+	schema: JSONSchema7 & { anyOf: JSONSchema7Definition[] },
+	refs: Refs
 ) => {
-  return schema.anyOf.length
-    ? schema.anyOf.length === 1
-      ? parseSchema(schema.anyOf[0], {
-          ...refs,
-          path: [...refs.path, "anyOf", 0],
-        })
-      : `z.union([${schema.anyOf.map((schema, i) =>
-          parseSchema(schema, { ...refs, path: [...refs.path, "anyOf", i] })
-        )}])`
-    : `z.any()`;
+	if (!schema.anyOf.length) {
+		return "z.any()";
+	} else if (schema.anyOf.length === 1) {
+		return parseSchema(schema.anyOf[0], {
+			...refs,
+			path: [...refs.path, "anyOf", 0],
+		});
+	} else {
+		let typeFiltered = schema.anyOf.filter((type) =>
+			typeof type === "object" ? its.a.primitive(type, "null") : true
+		);
+		let hasNull = typeFiltered.length < schema.anyOf.length;
+		let only = typeFiltered.length === 1 ? typeFiltered[0] : null;
+		if (only) {
+			return `${parseSchema(schema.anyOf[0], {
+				...refs,
+				path: [...refs.path, "anyOf", 0],
+			})}.nullable()`;
+		}
+		return `z.union([${schema.anyOf.map((schema, i) =>
+			parseSchema(schema, { ...refs, path: [...refs.path, "anyOf", i] })
+		)}])${hasNull ? '.nullable()' : ''}`;
+	}
 };
--- src/parsers/parseMultipleType.ts
+++ src/parsers/parseMultipleType.ts
@@ -1,12 +1,16 @@
 import { JSONSchema7, JSONSchema7TypeName } from "json-schema";
 import { Refs } from '../Types';
-import { parseSchema } from "./parseSchema";
+import { its, parseSchema } from "./parseSchema";
 
 export const parseMultipleType = (
   schema: JSONSchema7 & { type: JSONSchema7TypeName[] },
   refs: Refs
 ) => {
-  return `z.union([${schema.type.map((type) =>
+  let typeFiltered = schema.type.filter(type => type !== 'null');
+  let hasNull = typeFiltered.length < schema.type.length;
+  let only = typeFiltered.length === 1 ? typeFiltered[0] : null;
+
+  return only ? `${parseSchema({ ...schema, type: only })}.nullable()` :  `z.union([${typeFiltered.map((type) =>
     parseSchema({ ...schema, type }, refs)
-  )}])`;
+  )}])${hasNull ? ".nullable()" : ""}`;
 };
