2e1d82f395a560ecaf67711556141e8295425295
Kris <kristjan.grm1@gmail.com>
2023-10-25 23:53:32 +0200

    Update parseString.ts

--- src/parsers/parseString.ts
+++ src/parsers/parseString.ts
@@ -4,11 +4,15 @@ export const parseString = (schema: JSONSchema7 & { type: "string" }) => {
   let r = "z.string()";
   if (schema.pattern)
     r += `.regex(new RegExp(${JSON.stringify(schema.pattern)}))`;
+
+  if (schema.format === "date") return "z.coerce.date()";
+  
   if (schema.format === "email") r += ".email()";
   else if (schema.format === "uri") r += ".url()";
   else if (schema.format === "uuid") r += ".uuid()";
   else if (schema.format === "date-time") r += ".datetime()";
   if (typeof schema.minLength === "number") r += `.min(${schema.minLength})`;
   if (typeof schema.maxLength === "number") r += `.max(${schema.maxLength})`;
+
   return r;
 };
